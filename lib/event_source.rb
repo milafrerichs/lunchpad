640d74229388e385f41b745d77e45b561c1e886b
Thomas Hollstegge <thomas.hollstegge@zweitag.de>
2013-09-06 19:49:16 +0200

    Send connect event on start

--- lib/event_source.rb
+++ lib/event_source.rb
@@ -1,11 +1,13 @@
 class EventSource
   def initialize(io)
     @io = io
+
+    publish 'connect'
   end
 
   def publish(event, data={})
     @io.write "event: #{event}\n"
-    @io.write "data: #{data.to_json}\n"
+    @io.write "data: #{data.to_json}\n\n"
   end
 
   def close
