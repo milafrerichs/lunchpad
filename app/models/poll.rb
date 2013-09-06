3e5a948fc2922ddd31825acf7b51d6a642061472
Sam Figueroa <unimatrixzxero@gmail.com>
2013-09-06 17:41:25 +0200

    Amend Poll mock with hashie

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,19 +2,19 @@ class Poll
 
   def venues
     [
-      { name: 'CC', votes:
+      Hashie::Mash.new({ name: 'CC', votes:
         [
           { user: 'Micha', time: '12:15' },
           { user: 'Heiko', time: '12:15' },
           { user: 'Yasmin', time: '12:30' }
         ]
-      },
-      { name: 'Royals & Rice', votes:
+      }),
+      Hashie::Mash.new({ name: 'Royals & Rice', votes:
         [
           { user: 'Sam', time: '12:30' },
           { user: 'Lars', time: '12:30' }
         ]
-      }
+      })
     ]
   end
 
