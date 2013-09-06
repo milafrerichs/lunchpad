80a72181e5bafd1bc552370d5de036f000dbf19b
Heiko Zeus <heiko.zeus@zweitag.de>
2013-09-06 16:16:37 +0200

    rails new lunchpad

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lunchpad::Application.config.secret_key_base = 'c7cd10e608cc19b56ea707fd6674cb054cb07c07b26d30632221a00d9a6050f46b1f125f545d0a89d007522a9627a6d66d2fe18d0a0a62bba5c6c50e2c26ecc4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lunchpad::Application.config.session_store :cookie_store, key: '_lunchpad_session'
